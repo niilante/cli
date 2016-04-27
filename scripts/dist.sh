ecb23511e3955411ba6a9aa3a34b5e9207cafb5b
uzyexe <uzy.exe@gmail.com>
2016-04-27 15:12:31 +0900

    Fix scripts/dist.sh

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -24,6 +24,15 @@ for FILENAME in $(find ./pkg -mindepth 1 -maxdepth 1 -type f); do
     cp "./pkg/${FILENAME}" "./pkg/dist/arukas_${VERSION}_${FILENAME}"
 done
 
-ghr -t ${GITHUB_TOKEN} -u ${USERNAME} -r ${CIRCLE_PROJECT_REPONAME} --replace ${VERSION} dist/
+UPLOAD_URL=$(curl -v https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/${VERSION} | jq '.upload_url')
+UPLOAD_URL=$(echo ${UPLOAD_URL} | sed -e 's/{?name,label}/?name/g' | sed -e 's/"//g')
+
+for FILENAME in $(find ./pkg -mindepth 1 -maxdepth 1 -type f); do
+    FILENAME="$(basename "${FILENAME}")"
+    curl --data-binary @pkg/dist/arukas_${VERSION}_${FILENAME} \
+        -H "Content-Type: application/zip" \
+        -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+        "${UPLOAD_URL}=arukas_${VERSION}_${FILENAME}"
+done
 
 exit 0
